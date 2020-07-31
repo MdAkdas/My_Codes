8cd202e4f4b31820f59577c9983d7e42b3f5c5e4
MdAkdas <ahmadakdas07@gmail.com>
2020-07-31 23:32:51 +0530

    July31 LC

---
+++ leetcode/July_challenge/climbingstairs.cpp
@@ -0,0 +1,25 @@
+/*You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps*/
+
+class Solution {
+    int dp[50];
+public:
+    int climbStairs(int n) {
+        if(n==1 || n==2)
+            return n;
+        if(dp[n]!=0)
+            return dp[n];
+            
+         dp[n]= climbStairs(n-1) + climbStairs(n-2);
+        return dp[n];
+    }
+};
